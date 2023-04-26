9f76edfa00379f468cd86f8f8d3c1246c20a35b5
meiso3 <mli250@ucsc.edu>
2023-04-26 16:14:03 -0700

    lab 5 first changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,15 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors Pseudocode
+// Author: Christina Li
+// Date: April 2023
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function game(param1, param2) {
+
+  //Print images of 'rock' 'paper' and 'scissors' on the screen.
+  //Print a timer for each player (10 seconds).
+  //Timer starts counting down from 10 seconds:
+    //while seconds is greater than 0
+      //print the value in seconds
+      //subtract 1 from seconds
+  //Accept user input from both ends.
+    //If no user input is obtained from both players
+      //The game restarts.
+  //Whenthe timer reaches 0
+    //Print both user inputs on screen.
+  //Print the winner's username.
 }
 
 function main() {
